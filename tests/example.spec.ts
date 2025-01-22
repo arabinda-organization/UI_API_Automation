4e801056c93eeaf9322c0b72dd023f2aea58d504
ArabindaAutomation <68094626+ArabindaAutomation@users.noreply.github.com>
2025-01-23 00:27:50 +0530

    feat: added home page test (#1)
    * added home page test
    
    * added more valiation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -25,3 +25,14 @@ test('logintest', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+test('homepagetest', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
